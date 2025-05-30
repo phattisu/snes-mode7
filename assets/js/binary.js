a8a0d226bc834e2b0e3390d6c55ff277cdec388a
Phattisu <naphatism@hotmail.com>
2025-05-30 11:47:51 +0700

    1.1.3

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.56/---simulator","cdnUrl":"https://cdn.makecode.com","version":"1.1.2","target":"arcade","targetVersion":"2.0.56","repo":"phatisena/snes-mode7"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.56/---simulator","cdnUrl":"https://cdn.makecode.com","version":"1.1.3","target":"arcade","targetVersion":"2.0.56","repo":"phatisena/snes-mode7"}
 // total=1635150 new=28.91% cached=66.79% other=4.30%
 (function (ectx) {
 'use strict';
@@ -428,7 +428,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P26198(s) {
+function _main___P18143(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -830,13 +830,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P26198.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P26198.continuations = [  ]
+_main___P18143.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P18143.continuations = [  ]
 
-function _main___P26198_mk(s) {
+function _main___P18143_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P26198, depth: s.depth + 1,
+        parent: s, fn: _main___P18143, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42058,5 +42058,5 @@ const sprites_BaseSpriteSayRenderer__C2273_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P26198
+return _main___P18143
 })
